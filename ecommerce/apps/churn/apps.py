dd38e6934b18880b0557e45020b62b7bf3379ac4
karina-p1 <karinapaudel821@gmail.com>
2026-05-10 14:36:54 +0545

    Refactor app names to include 'apps.' prefix and add initial migration for custom User model; update settings for static and media files; create base and home templates.

--- ecommerce/apps/churn/apps.py
+++ ecommerce/apps/churn/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ChurnConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'churn'
+    name = 'apps.churn'
--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,9 +130,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
