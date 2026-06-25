dc285e6252f272aa1d19ffb159763baa96479c9f
Preeti Gurung <gurungpreeti999@gmail.com>
2026-06-25 16:34:15 +0545

    feat: update URL routing for dashboard and add logout button in admin template

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.dashboard.urls')),
+    path('dashboard/', include('apps.dashboard.urls')),
     path('accounts/', include('apps.accounts.urls')),
     path('analytics/', include('apps.analytics.urls')),
     path('activity/', include('apps.activity.urls')),
