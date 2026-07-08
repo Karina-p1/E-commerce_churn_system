2a382b3322975147192bda1b9e4a12f232106c4d
karina-p1 <karinapaudel821@gmail.com>
2026-07-08 15:05:25 +0545

    Enhance EDA notebook with detailed univariate and bivariate analysis insights; update context processor to order categories and brands by name and order respectively.

--- ecommerce/apps/products/context_processors.py
+++ ecommerce/apps/products/context_processors.py
@@ -3,6 +3,6 @@ from .models import Category, Brand
 
 def navbar_data(request):
     return {
-        'all_categories': Category.objects.all(),
-        'all_brands': Brand.objects.all(),
+        'all_categories': Category.objects.all().order_by('name'),
+        'all_brands': Brand.objects.all().order_by('order', 'name'),
     }
