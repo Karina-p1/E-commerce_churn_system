d66ea33d81e2f7007294fdab7142cbcf5b97a44a
grgsaugat11 <saugat987.sg@gmail.com>
2026-07-12 16:31:50 +0545

    feat(activity): add UserSession model and implement activity tracking with inactivity timer

--- ecommerce/apps/activity/urls.py
+++ ecommerce/apps/activity/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import log_click
-
+from . import views
 
 app_name = 'activity'
 
@@ -10,5 +10,10 @@ urlpatterns = [
         'click/<int:product_id>/',
         log_click,
         name='log_click'
+    ),
+    path(
+        "ping/",
+        views.activity_ping,
+        name="activity_ping",
     ),
 ]
